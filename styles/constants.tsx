384f34156a616f52c3bff156f8674bdb1578767a
antonlangbruttig <antonlangbruttig@gmail.com>
2024-07-17 13:04:55 -0400

    Update to navbar

---
+++ styles/constants.tsx
@@ -0,0 +1,11 @@
+import { Icon } from '@iconify/react'
+
+import {SideNavItem} from './types';
+
+export const SIDENAv_ITEMS: SideNavItem [] = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <Icon icon="lucide:home" width="24" height="24" />,
+    },
+];
